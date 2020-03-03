93e416b68b1a7e3662277ade2c86d0e98cd15afa
“kulkarohan” <kulkarohan@users.noreply.github.com>
2020-03-03 15:59:15 -0500

    heroku configuration

--- cs_mud/settings.py
+++ cs_mud/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
+import dj_database_url
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -95,6 +97,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES['default'] = dj_database_url.config(
+#     default=config('DATABASE_URL')
+# )
 
 
 # Password validation
